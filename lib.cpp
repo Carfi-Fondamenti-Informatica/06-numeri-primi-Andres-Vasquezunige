15a5cfb86de50d9dea3fa29874b5b19debf9d33b
Andres-Vasquezunige <116440246+Andres-Vasquezunige@users.noreply.github.com>
2022-12-02 19:20:42 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool numeroprimo(int a, int b){
+    if(a % b == 0){
+        if(a == b){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    numeroprimo(a, b + 1);
+}
